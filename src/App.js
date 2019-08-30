5ed7c732e3efcf00bf0e30999df5b654bb010a0e
joao tavares <joao.arthur.tavares@gmail.com>
2019-08-29 23:00:38 -0300

    first blood

---
+++ src/App.js
@@ -0,0 +1,104 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+import {
+  phase1,
+  phase2,
+  phase3,
+} from './questions';
+
+const Container = styled.div`
+width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  color: seashell;
+  background-color: cornflowerblue;
+`;
+const Content = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  padding: 2rem;
+`;
+
+const Question = styled.h1`
+  font-size: 28px;
+  text-align: center;
+`;
+
+const NextButton = styled.button`
+    font-size: 16px;
+    background-color: rgba(100, 149, 237, 0.4);
+    color: seashell;
+    border-radius: 32px;
+    padding: 0.5rem;
+`;
+
+const generateQuestion = () => {
+  let step1 = phase1;
+  let step2 = phase2;
+  let step3 = phase3;
+
+  if (step1.length >= 1) {
+    const randomIndex = Math.floor(Math.random() * step1.length -1 ) + 1;
+    console.log("Step1: ", randomIndex);
+    const question = step1[randomIndex]
+    step1.splice(randomIndex, 1)
+    return question
+  } else if (step2.length >= 1) {
+    const randomIndex = Math.floor(Math.random() * step2.length -1 ) + 1;
+    console.log("Step2: ", randomIndex);
+    const question = step2[randomIndex]
+    step2.splice(randomIndex, 1)
+    return question
+  } else if (step3.length >= 1){
+    const randomIndex = Math.floor(Math.random() * step3.length -1 ) + 1;
+    console.log("Step3: ", randomIndex);
+    const question = step3[randomIndex]
+    step3.splice(randomIndex, 1)
+    return question
+  }
+
+  return 'Obrigado por compartilharem um pouquinho de vocês, aproveitem a noite!'
+}
+
+const App = () => { 
+  const [question, setNextQuestion] = useState('36 perguntas sobre nós');
+
+  return (
+    <Container >
+      {
+        question !== 'Gran Finale!' 
+        ? (
+        <Content>
+          <div/>
+          <Question>{question}</Question>
+          {
+            question !== '36 perguntas sobre nós'
+            ? <NextButton onClick={() => {
+                question !== 'Obrigado por compartilharem um pouquinho de vocês, aproveitem a noite!'  
+                  ? setNextQuestion(generateQuestion()) 
+                  : setNextQuestion('Gran Finale!')
+              }}>Próxima Pergunta</NextButton>
+            : <NextButton onClick={() => setNextQuestion(generateQuestion())}>Vamos começar!</NextButton>
+          }
+        </Content>
+        ) : (
+          <Content>
+          <Question>Agora é hora de se conhecer sem palavras</Question>
+          <Question>Hora de olhar nos olhos um do outro pro 2 minutos</Question>
+          <NextButton onClick={() => window.location.reload()}>Jogar novamente!</NextButton>
+        </Content>
+        )
+
+      }
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/questions.js
@@ -0,0 +1,49 @@
+const phase1 = [
+    'Se você pudesse escolher qualquer pessoa do mundo, quem você iria querer como convidado para um jantar?',
+    'Você gostaria de ser famoso? De que forma?',
+    'Antes de fazer um telefonema, você costuma ensaiar o que vai dizer? Por quê?',
+    'O que constitui um dia “perfeito” para você?',
+    'Quando foi a última vez que você cantou sozinho? E para outra pessoa?',
+    'Se você viver até os 90 anos e se pudesse escolher entre manter o corpo ou a mente dos seus 30 pelos últimos 60 anos de sua vida, qual você escolheria?',
+    'Você tem algum pressentimento sobre como você vai morrer?',
+    'Cite três coisas que você e seu (sua) parceiro(a) parecem ter em comum',
+    'Pelo que você se sente mais grato em sua vida?',
+    'Se você pudesse mudar algo sobre a forma de como foi criado, o que seria?',
+    'Conte a seu (sua) parceiro(a), em quatro minutos, a história de sua vida da maneira mais detalhada possível',
+    'Se você pudesse acordar amanhã com uma qualidade ou habilidade nova, qual seria?'
+];
+
+const phase2 = [
+    'Se uma bola de cristal pudesse revelar a verdade sobre você mesmo, sua vida, seu futuro ou qualquer outra coisa, o que você gostaria de saber?',
+    'Há algo que você sonha em fazer há muito tempo? Por que ainda não o fez?',
+    'Qual é a maior realização da sua vida?',
+    'O que você mais valoriza em uma amizade?',
+    'Qual é a sua lembrança mais preciosa?',
+    'Qual é a sua lembrança mais terrível?',
+    'Se você soubesse que em um ano você irá morrer subitamente, você mudaria alguma coisa sobre a forma de como está vivendo agora? Por quê?',
+    'O que a amizade significa para você?',
+    'Que papel o amor e a afeição desempenham em sua vida?',
+    'Alternando as respostas, diga cinco coisas que você considera características positivas do seu (sua) parceiro(a). Mencione um total de cinco características.',
+    'O quanto a sua família é próxima e afetuosa? Você sente que sua infância foi mais feliz que a de outras pessoas?',
+    'Como você se sente sobre o relacionamento com a sua mãe?',
+];
+const phase3 = [
+    'Faca três declarações cada um, usando fatos verdadeiros e “nós dois” na mesma frase. Por exemplo, “Nós dois estamos nesta sala sentindo...”',
+    'Complete a frase: “Eu gostaria de ter alguém com quem eu pudesse dividir...”',
+    'Se você fosse se tornar um amigo próximo de seu (sua) parceiro(a), compartilhe o que seria importante que ele ou ela soubesse.',
+    'Diga a seu (sua) parceiro(a) o que você gosta nele; seja bem honesto desta vez, dizendo coisas que você não diria a alguém que tivesse acabado de conhecer.',
+    'Compartilhe com seu (sua) parceiro(a) um momento embaraçoso de sua vida.',
+    'Quando foi a última vez que você chorou na frente de outra pessoa? E sozinho?',
+    'Diga a seu (sua) parceiro(a) algo que você já gosta nele(a).',
+    'O que você acha sério demais, se é que há algo, que nunca deveria ser motivo de chacota ou piada?',
+    'Se você fosse morrer esta noite sem ter a oportunidade de se comunicar com ninguém, do que você se arrependeria de não ter dito? Por que você ainda não disse isso a essa pessoa?',
+    'Sua casa, com tudo o que você possui, pega fogo. Depois de salvar todas as pessoas e animais, você tem tempo para uma volta final, com segurança, para salvar qualquer outro objeto. O que seria? Por quê?',
+    'De todas as pessoas de sua família, a morte de quem você consideraria mais arrasadora?',
+    'Compartilhe um problema pessoal e peça o conselho de seu (sua) parceiro(a) sobre como ele(a) lidaria com isso. Também peça a seu (sua) parceiro(a) para descrever a impressão dele(a) de como você parece se sentir quando fala sobre o problema que escolheu.',
+];
+
+export {
+    phase1,
+    phase2,
+    phase3,
+}
